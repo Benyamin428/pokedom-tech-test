508c4ea00418275150360ca0e90e45686190a0a6
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2022-07-06 15:19:40 +0100

    Completed extension challenge. Included filtering for pokemons

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import { pokemonArray } from './data/pokemon.js';
 console.log(pokemonArray);
 
 const cardContainer = document.querySelector('.card-container');
+const input = document.querySelector('#input');
+const searchButton = document.querySelector('#search-button');
+const noOfPokemons = document.querySelector('#pokemons');
 
 const checkContents = (pokemon, index) => {
     if (pokemon.types.length == 1) {
@@ -13,8 +16,8 @@ const checkContents = (pokemon, index) => {
     }
 }
 
-pokemonArray.forEach((pokemon, index) => {
-    cardContainer.innerHTML += `
+const displayCard = (pokemon, index) => {
+    return cardContainer.innerHTML += `
     <div class="card"> 
     <img src=${pokemon.sprite} class="card__image">
     <div class="card__content">
@@ -22,4 +25,42 @@ pokemonArray.forEach((pokemon, index) => {
         ${checkContents(pokemon, index+1)}
     </div>
     </div>`
-})
+}
+
+const search = (event) => {
+    cardContainer.innerHTML = "";
+    console.log(input.value)
+    const filteredPokemon = pokemonArray.filter((pokemon) => {
+        //name filter
+        if (input.value.toLowerCase() == pokemon.name) {
+            return pokemon;
+        }
+        //tag filter
+        if (input.value.toLowerCase() == pokemon.types[0] || input.value.toLowerCase() == pokemon.types[1] ) {
+                return pokemon;
+        }
+    })
+    filteredPokemon.forEach((pokemon, index) => {
+        displayCard(pokemon, index);
+    })
+}
+
+const changeNoOfPokemons = (event) => {
+    console.log(event.target.value);
+    cardContainer.innerHTML = "";
+    const newList = pokemonArray.slice(0, parseInt(event.target.value));
+    newList.forEach((pokemon, index) => {
+        displayCard(pokemon, index);
+    })
+}
+
+const displayAllCards = () => {
+    pokemonArray.forEach((pokemon, index) => {
+        displayCard(pokemon, index);
+    })
+}
+
+searchButton.addEventListener("click", search);
+noOfPokemons.addEventListener("change", changeNoOfPokemons);
+
+displayAllCards();
